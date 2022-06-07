d664bf54dc1164176a85d8a7763a7fc6dc505076
Elrond <ozturknuri8qgmail.com>
2022-06-07 00:15:22 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+import numpy as np
+from PIL import Image
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['UPLOAD_FOLDER'] = "static/images/"
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+def new_get_colors(pic, num):
+    hexalist = []
+    img = Image.open(pic)
+    img_array = np.array(img)
+    for color in img_array:
+        rgb = tuple(color[1])
+        hexacode = '%02x%02x%02x' % rgb
+        if hexacode not in hexalist:
+            hexalist.append(hexacode)
+    length = len(hexalist)
+    step = int(length / num)
+    return hexalist[:length:step]
+
+
+@app.route("/")
+def home():
+    home_list = ['dde4e0', 'aac9de', '625217', '24291c', '8db9da', '5f6159', '74aed8', '976b0e', 'd8a10e', '4e360e']
+    return render_template("index.html", home_list=home_list)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(app.config['UPLOAD_FOLDER'] + secure_filename(f.filename))
+        path = app.config['UPLOAD_FOLDER'] + secure_filename(f.filename)
+        number = request.values['number']
+        colors = new_get_colors(path, int(number))
+        return render_template("index.html", uploaded_image=f.filename, list=colors)
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename='images/' + filename), code=301)
+    # return send_from_directory(app.config["IMAGE_UPLOADS"], filename)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
---
+++ static/index.js
@@ -0,0 +1,42 @@
+function clip_text(a_string){
+    var input = document.createElement('input')
+    input.id="__copyText__";
+    input.value = a_string; // OOPS! document.getElementById(divId).innerText;
+    //document.body.appendChild(input);
+    input.select();
+
+    input.setSelectionRange(0, 999)
+    navigator.clipboard.writeText(input.value)
+  //copyText.select();
+  //copyText.setSelectionRange(0, 99999);
+  //navigator.clipboard.writeText(copyText.value);
+
+  //  document.execCommand("copy");
+  //  var txt = input.value
+  //  input.remove()
+}
+function clip_div(divId){
+   let table = document.querySelector(".table");
+   table.addEventListener('click', function(event) {
+    	// event.target = '<td>', so we pick it's parent
+    	const row = event.target.parentNode.parentNode;
+    	const raf = row.innerText.split(' ')
+    	const leo = raf[0].substring(2, 10)
+        //console.log(leo)
+    	// row = '<tr>'
+    	return clip_text(leo)
+    });
+
+}
+const buttons = document.querySelectorAll(".copy")
+buttons.forEach((btn) => {
+  btn.addEventListener('click', function(e){
+    if(btn.innerText = "Copy"){
+        btn.innerText = "Done";
+    }else{
+    btn.innerText="Copy";
+    }
+
+
+  })
+})
